895e7cd5e1691ea7ce048f808472a49f9f913628
atm-shfiat <shifatatm@gmail.com>
2025-02-12 18:59:40 +0600

    project set up

---
+++ src/app/(rs)/layout.tsx
@@ -0,0 +1,17 @@
+import { Header } from "@/components/Header";
+import { ThemeProvider } from "@/components/theme-provider";
+
+export default async function RSLayout({
+    children,
+} : { 
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="mx-auto w-full max-w-7xl">
+            <Header />
+            <div className="px-4 py-2">
+                {children}
+            </div>
+        </div>    
+    )
+}
